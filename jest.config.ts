e8de4aea0f62e892603e41297a465c8e21c32286
Peter Goeren <petergoeren@Peters-Mac-mini.local>
2026-03-29 19:33:17 -0600

    chore: initialize project scaffold

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  collectCoverageFrom: ['src/**/*.ts'],
+};
+
+export default config;
